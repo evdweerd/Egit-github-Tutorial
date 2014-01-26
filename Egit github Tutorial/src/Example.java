906e8c383f495d411a7747e0ca7f4f99d1196779
evdweerd <evdweerd@gmail.com>
2014-01-26 15:36:05 +0100

    Update Example.java
    v2

--- Egit github Tutorial/src/Example.java
+++ Egit github Tutorial/src/Example.java
@@ -1,11 +1,10 @@
-
 public class Example {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 2
 
 	}
 
